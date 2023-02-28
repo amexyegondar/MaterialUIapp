c62e81b3762b4ced8b049034dbd361d90afdf7cc
amexyegondar <shibabawamanual@gmail.com>
2023-02-28 09:38:28 -0800

    social-media

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from '@mui/material/Button'
+import DeleteIcon from ''
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button startIcon={<DeleteIcon/>} variant="contained" color='secondary'>Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
